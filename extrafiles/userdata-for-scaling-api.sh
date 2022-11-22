3ea7d500cbd35db66a066ec5d0187233fcd5a2ad
bharath kumar <bharathkumar@bharaths-MacBook-Air.local>
2022-11-22 16:44:17 +0530

    updated for ubuntu

--- extrafiles/userdata-for-scaling-api.sh
+++ extrafiles/userdata-for-scaling-api.sh
@@ -5,15 +5,15 @@ exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
 
 # Install Git
 echo "Installing Git"
-yum update -y
-yum install git -y
+apt update -y
+apt install git -y
 
 # Install NodeJS
 echo "Installing NodeJS"
 touch .bashrc
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. /.nvm/nvm.sh
-nvm install --lts
+source /.nvm/nvm.sh
+nvm install 14.21.1
 
 # Clone website code
 echo "Cloning website"
